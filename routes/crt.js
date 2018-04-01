d732ce87b7e169cff4d6f02b7885d0f1290c3f6e
Maxmya <maxmya@outlook.com>
2018-04-01 20:57:11 +0200

    fix typo

--- routes/crt.js
+++ routes/crt.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 router.get('/', function (req, res, next) {
-    
+
     res.render('crt');
 
 
@@ -19,14 +19,29 @@ router.post('/', function (req, res, next) {
     let m3 = req.body.m3;
 
 
-   
-    
+
+
 
 });
 
 
-function solveLinearCongurence(a,b,c,m1,m2,m3){
+function solveModulo(a, b, c, m1, m2, m3) {
 
-}
+};
+
+
+function egcd(a, b) {
+    if (a == 0)
+        return b;
+
+    while (b != 0) {
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    }
+
+    return a;
+};
 
 module.exports = router;
