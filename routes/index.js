9ea2ea4cc156754f7e7e2e175ff3273d5b365188
Mahmoud Aref <maxmya@outlook.com>
2018-06-18 15:32:33 +0100

    it's expressjs freamworke

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
   res.render('index', {
     title: 'Number Theory',
-    paragraph: 'Number theory class 2018 programming task , this website is made with "nodejs" a javascript freamwork for web.\nMade by : Mahmoud Aref , Code : 110747'
+    paragraph: 'Number theory class 2018 programming task , this website is made with "expressjs" a javascript freamwork for web.\nMade by : Mahmoud Aref , Code : 110747'
   });
 });
 
